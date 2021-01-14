9e6b2bde06cfca89f5f3fe75129354d94e96b916
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-14 13:23:39 -0600

    Coded random questions in js file and added a new class for question div in question.html file.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // DOM elements
-var startBtn = $("#start");
-var choice1Btn = $("#choice-1");
-var choice2Btn = $("#choice-2");
-var choice3Btn = $("#choice-3");
-var choice4Btn = $("#choice-4");
-var submitBtn = $("#submit");
+const startBtn = $("#start");
+const choices = Array.from(document.querySelectorAll (".choice-text"));
+const submitBtn = $("#submit");
 var question = $("h1")
 
 //Variables
@@ -15,7 +12,40 @@ let nextQuestion= [];
 
 let questions = [
     {
-        question: " "
+        question: "What is 2 + 2? ",
+        choice1:"2",
+        choice2:"3" ,
+        choice3:"5",
+        choice4:"8",
+        answer: 2,
+        
+    },
+    {
+        question: "What is 2 + 3? ",
+        choice1:"2",
+        choice2:"3" ,
+        choice3:"5",
+        choice4:"8",
+        answer: 5,
+        
+    },
+    {
+        question: "What is 2 + 5? ",
+        choice1:"2",
+        choice2:"10" ,
+        choice3:"7",
+        choice4:"8",
+        answer: 7,
+        
+    },
+    {
+        question: "What is 2 + 4? ",
+        choice1:"2",
+        choice2:"6" ,
+        choice3:"5",
+        choice4:"8",
+        answer: 6,
+        
     }
 ]
 
@@ -33,8 +63,14 @@ $("<a>")
 
 // Funtions 
 
-function startGame(){
+startGame = () => {
+score = 0
+nextQuestion= [...questions]
+getNewQuestion()
+}
 
+getNewQuestion = () => {
+    if(nextQuestion.length === 0 || )
 }
 
 function selectAnswer(){
