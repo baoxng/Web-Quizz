984358725c0b487a8cfc794612cea191cc2d1b69
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-13 20:55:18 -0600

    Psuedo code for script.js and added variables.

---
+++ script.js
@@ -0,0 +1,40 @@
+// DOM elements
+var startBtn = $("#start");
+var choice1Btn = $("#choice-1");
+var choice2Btn = $("#choice-2");
+var choice3Btn = $("#choice-3");
+var choice4Btn = $("#choice-4");
+var submitBtn = $("#submit");
+
+//Click-Event
+$("#start").click(function(){
+console.log("started")
+
+
+})
+
+
+
+// Funtions 
+
+function startGame(){
+
+}
+
+function selectAnswer(){
+
+}
+
+
+
+//Array of Questions
+
+
+
+
+//Timer: When timer hits zero then stop quiz.
+
+
+
+
+//Submit Scores and save player's name 
